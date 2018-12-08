cf43d02778a7c3d75f29ab0a73ff81a81094d97c
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-12-08 11:50:44 +0900

    feat(store): build data structure

--- store/index.ts
+++ store/index.ts
@@ -2,7 +2,64 @@ import { db } from '~/plugins/firebase'
 import { firebaseMutations, firebaseAction } from 'vuexfire'
 const usersRef = db.collection('users')
 export const state = (): object => ({
-  users: [],
+  organizations: [{
+    id: 'mT91Dcvq9SPyrK1bxemZ',
+    administrators: ['uk9c9RcfPenLLiSCY1MX'],
+    members: [],
+    default_team_beggining_wday: 1,
+    default_team_holiday_wdays: [0, 6],
+    createdAt: '2018-12-07T00:00:00.000Z',
+    updatedAt: '2018-12-08T12:30:00.000Z',
+  }],
+  teams: [{
+    id: 'enli3dIfHv20kr3O5oPZ',
+    name: 'a-team',
+    administrators: ['uk9c9RcfPenLLiSCY1MX'],
+    members: [],
+    followers: [],
+    beggining_wday: 1,
+    holiday_wdays: [0,6],
+    lables: [{
+      name: 'team_label',
+      color: '#ff0000'
+    }],
+    createdAt: '2018-12-07T00:00:00.000Z',
+    updatedAt: '2018-12-08T12:30:00.000Z',
+  }],
+  users: [{
+    id: 'uk9c9RcfPenLLiSCY1MX',
+    name: 'a-user',
+    default_monologue_range: 'organization',
+    default_task_range: 'team',
+    labels: [{
+      name: 'user_label',
+      color: '#00ff00'
+    }],
+    createdAt: '2018-12-07T00:00:00.000Z',
+    updatedAt: '2018-12-08T12:30:00.000Z',
+  }],
+  tasks: [{
+    id: 'brG23RrTsPotVArHsxMG',
+    title: 'task_title',
+    description: 'task_description',
+    user: 'uk9c9RcfPenLLiSCY1MX',
+    range: 'enli3dIfHv20kr3O5oPZ',
+    current_week: true,
+    labels: [{
+      name: 'user_label',
+      color: '#00ff00'
+    }],
+    createdAt: '2018-12-07T00:00:00.000Z',
+    updatedAt: '2018-12-08T12:30:00.000Z',
+  }],
+  monologues: [{
+    id: 'yc2Ayly5T13b8VhhWkWe',
+    user: 'uk9c9RcfPenLLiSCY1MX',
+    range: 'mT91Dcvq9SPyrK1bxemZ',
+    line: 'happy',
+    createdAt: '2018-12-07T00:00:00.000Z',
+    updatedAt: '2018-12-08T12:30:00.000Z',
+  }]
 })
 
 export const getters = {
