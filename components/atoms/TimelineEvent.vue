fe88653d3cbb7abd223c66c3e8d65f05129abae5
Aqui Tsuchida <aqui.tcd+dev@gmail.com>
2018-12-23 15:19:34 +0900

    feat(*): add components for timeline

--- components/atoms/TimelineEvent.vue
+++ components/atoms/TimelineEvent.vue
@@ -1,18 +1,53 @@
 <template lang="pug">
-  div
-    p {{event.user}}
-    p {{event.line}}
+  div.event
+    div.user
+      figure.avatar.member-avatar(:data-initial="member.name | toInitialName" :class="{hasAvatar: member.avatarUrl}")
+        img(:src="member.avatarUrl" v-if="member.avatarUrl")
+      p.name {{member.name}}
+    p.line {{event.line}}
+    .timestamp
+      time {{ this.event.createdAt.toDate() | toDatetimeMMDDHHMM }}
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
-import MyInfo from '~/components/atoms/MyInfo.vue'
-import MyMonologue from '~/components/atoms/MyMonologue.vue'
+import { Getter } from 'vuex-class'
 @Component
-export default class Me extends Vue {
+export default class TimelineEvent extends Vue {
   @Prop(Object) event?
+  @Getter getMemberById
+  get member() {
+    return this.getMemberById(this.event.memberId)
+  }
 }
 </script>
 
 <style scoped lang="sass">
+.event
+  padding: 8px
+.user
+  display: flex
+  flex-direction: row
+  align-items: center
+  justify-content: left
+  width: 100%
+  padding: 0 8px
+.member-avatar
+  background-color: $gray-color
+  &.hasAvatar
+    background-color: transparent
+.name
+  align-self: flex-start
+  font-weight: bold
+  padding-left: 13px
+  margin-bottom: 0
+.line
+  padding: 8px
+  font-size: 0.8em
+  margin-bottom: 0
+.timestamp
+  font-size: 0.8em
+  margin-bottom: 0
+  text-align: right
+  color: $gray-color-dark
 </style>
--- components/organisms/TheTimeline.vue
+++ components/organisms/TheTimeline.vue
@@ -1,26 +1,26 @@
 <template lang="pug">
   div
-    Me(:me="me")
-    TimelineEvent(v-for="event in events"
+    TimelineEvent.timeline_event(v-for="event in events"
       :event="event"
       :key="event.id")
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'nuxt-property-decorator'
-import Me from '~/components/molecules/Me.vue'
 import TimelineEvent from '~/components/atoms/TimelineEvent.vue'
 @Component({
   components: {
-    Me,
     TimelineEvent,
   },
 })
 export default class TheTimeline extends Vue {
-  @Prop(Object) me?
   @Prop(Array) events?: Object
 }
 </script>
 
 <style scoped lang="sass">
+.my_line
+  border-bottom: $border-color solid 3px
+.timeline_event
+  border-bottom: $border-color solid 1px
 </style>
